a7025fab826feaea6d6738718fb3ceb445813457
Matias Mattamala <mmattamala@ug.uchile.cl>
2018-08-29 23:50:17 -0300

    Update smbot.py

--- smbot.py
+++ smbot.py
@@ -12,35 +12,17 @@ bot = telepot.Bot(bot_key)
 
 # this method handles the messages and prepares the sm-bot' response
 def handle(msg):
+	# get the chat id
 	chat_id = msg["chat"]["id"]
+	
+	# get the messages and turn them into lowercase
 	msj = msg["text"]
 	msj = msj.lower()
-	if "hola" in msj:
-		bot.sendMessage(chat_id, "Hello, I'm NAO. My internet address is 192-168-21-128")
-	elif "mmm" in msj:
-		bot.sendMessage(chat_id, "Fire")
-	elif "desastre" in msj or "terrible" in msj or "teggible" in msj:
-		bot.sendMessage(chat_id, "Visual Attacker")
-	elif "calor" in msj:
-		bot.sendMessage(chat_id, "Fire, exclamation mark")
-	elif "ofensivo" in msj: 
-		bot.sendMessage(chat_id, "UChile Lib")
-	elif "pucha" in msj:
-		bot.sendMessage(chat_id, "Segmentation Fault")
-	elif "rip" in msj:
-		bot.sendMessage(chat_id, "Low battery")
-	elif "robocup" in msj:
-		bot.sendMessage(chat_id, )
-	elif "tesis" in msj:
-		ans = random.randint(0,3)
-		if ans == 0:
-			bot.sendPhoto(chat_id,"url")
-		elif ans == 1:
-			bot.sendPhoto(chat_id,"url")
-		elif ans == 2:
-			bot.sendPhoto(chat_id,"url")
-		elif ans == 3:
-			bot.sendPhoto(chat_id,"url")
+	
+	# prepare response depending on the available words within the message
+	if "maquina" in msj:
+		bot.sendMessage(chat_id, "Fascista!")
+
 
 MessageLoop(bot, handle).run_as_thread()
 
